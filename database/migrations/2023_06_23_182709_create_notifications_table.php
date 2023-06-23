3939d6af637c5501d3db9b4ecd763459d4ebc0af
Ahmed01088 <ahmed.alharith01@gmail.com>
2023-06-23 21:29:15 +0300

    first commit

--- database/migrations/2023_06_23_182709_create_notifications_table.php
+++ database/migrations/2023_06_23_182709_create_notifications_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('notifications');
         Schema::create('notifications', function (Blueprint $table) {
 
             $table->id();
